de60dd248f45f7c22e8c47f2dfa43c3c35ede593
dave1725 <davedmj1725@gmail.com>
2026-01-06 00:27:22 +0530

    refactor(schema-index): index all created schema's

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,7 +1,29 @@
 import {schedule} from './schedule';
+import {blockContent} from './blockContent';
+import {aboutPage} from './aboutPage';
+import {person} from './person';
+import {organizer} from './organizer';
+import {faq} from './faq';
+import {announcement} from './announcement';
+import {prize} from './prize';
+import {sponsor} from './sponsor';
+import {rulesPage} from './rulesPage';
+import {siteSettings} from './siteSettings';
 
 export const schemaTypes = [
+    // Reusable types
+    blockContent,
+    // Documents
     schedule,
+    aboutPage,
+    person,
+    organizer,
+    faq,
+    announcement,
+    prize,
+    sponsor,
+    rulesPage,
+    siteSettings,
 ];
 
 
